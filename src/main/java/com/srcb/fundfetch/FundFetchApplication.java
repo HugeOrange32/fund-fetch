e894aab622fe05bc8ba7d9b3c45adeeda6af89f9
FFYzz <cryptochen95@gmail.com>
2020-08-06 09:22:22 +0800

    init project

---
+++ src/main/java/com/srcb/fundfetch/FundFetchApplication.java
@@ -0,0 +1,13 @@
+package com.srcb.fundfetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundFetchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundFetchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/srcb/fundfetch/FundFetchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srcb.fundfetch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundFetchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
